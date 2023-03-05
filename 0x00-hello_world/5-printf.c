581113e32f49463b027fc3f8a80eee2ec552753a
TembaNkabi <mrnkabi@gmail.com>
2023-03-05 02:04:28 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-".5-printf.c.swp"
+#include <stdio.h>
+/**
+ * main - Prints a line using the printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
